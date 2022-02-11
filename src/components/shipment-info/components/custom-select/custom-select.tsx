9b5905efb6e9c6f3cbf71b7586821d5f3cad6e06
RanelGM <mr.rr.coon@gmail.com>
2022-02-11 11:15:27 +0300

    Добавит стилизацию и логику поведения кастомного селекта

--- src/components/shipment-info/components/custom-select/custom-select.tsx
+++ src/components/shipment-info/components/custom-select/custom-select.tsx
@@ -1,31 +1,143 @@
-import { PackageTypeGroup } from "utils/const";
+import {
+  KeyboardEvent as ReactKeyboardEvent,
+  MouseEvent as ReactMouseEvent,
+  useEffect,
+  useState,
+} from "react";
+import { PackageLabel } from "types/product";
+import { PackageGroup } from "utils/const";
 
-const options = Object.values(PackageTypeGroup);
+const options = Object.values(PackageGroup);
+
+const getValueFromLabel = (label: PackageLabel | null) => {
+  if (label === null) {
+    return undefined;
+  }
+
+  const option = options.find((item) => item.label === label);
+  const value = option ? option.value : option;
+  return value;
+};
 
 function CustomSelect(): JSX.Element {
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+  const [selectedLabel, setSelectedLabel] = useState<PackageLabel | null>(null);
+  const isOptionSelected = selectedLabel !== null;
+  const selectedValue = getValueFromLabel(selectedLabel);
+
+  const handleDocumentClick = (evt: MouseEvent) => {
+    const target = evt.target as HTMLElement;
+    const isCustomSelectElement = target.closest(".custom-select");
+
+    if (!isCustomSelectElement) {
+      setIsSelectOpen(false);
+    }
+  };
+
+  const handleEscKeydown = (evt: KeyboardEvent) => {
+    if (evt.key === "Escape" && isSelectOpen) {
+      setIsSelectOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick);
+    document.addEventListener("keydown", handleEscKeydown);
+
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+      document.removeEventListener("keydown", handleEscKeydown);
+    };
+  });
+
+  const handleButtonClick = () => {
+    setIsSelectOpen(!isSelectOpen);
+  };
+
+  const handleSelectClick = (evt: ReactMouseEvent) => {
+    const itemElement = evt.target as HTMLLIElement;
+    const option = itemElement.getAttribute(
+      "data-select-label"
+    ) as typeof selectedLabel;
+
+    if (!option) {
+      return;
+    }
+
+    setSelectedLabel(option);
+    setIsSelectOpen(false);
+  };
+
+  const handleSelectKeydown = (evt: ReactKeyboardEvent) => {
+    const itemElement = evt.target as HTMLLIElement;
+    const isEnterKeydown = evt.key === "Enter";
+    const isTabKeydown = evt.key === "Tab";
+    const isTabReturn = evt.shiftKey && evt.key === "Tab";
+
+    const option = itemElement.getAttribute(
+      "data-select-label"
+    ) as typeof selectedLabel;
+
+    if (!option || (!isEnterKeydown && !isTabKeydown)) {
+      return;
+    }
+
+    if (isEnterKeydown) {
+      setSelectedLabel(option);
+      setIsSelectOpen(false);
+      return;
+    }
+
+    const isFirstOption = options.slice(0, 1).shift()?.label === option;
+    const isLastOption = options.slice(-1).pop()?.label === option;
+
+    if ((isFirstOption && isTabReturn) || (isLastOption && !isTabReturn)) {
+      setIsSelectOpen(false);
+    }
+  };
+
   return (
-    <div className="custom-select" data-select="custom-select">
-      <span className="custom-select__placeholder">Тип упаковки</span>
-      <span className="custom-select__error">Выберите тип упаковки</span>
-      <button className="custom-select__button" type="button">
-        <span className="visually-hidden">Выбрать тип упаковки</span>
+    <div
+      className={`custom-select ${
+        isSelectOpen ? "custom-select--opened" : ""
+      } ${isOptionSelected ? "custom-select--selected" : ""}`}
+      data-select="custom-select"
+    >
+      <button
+        className="custom-select__button"
+        type="button"
+        onClick={handleButtonClick}
+      >
+        <span className="custom-select__placeholder">Тип упаковки</span>
         <span className="custom-select__icon" />
+        <span className="custom-select__text">{selectedLabel}</span>
       </button>
       <ul className="custom-select__list" role="listbox">
-        {options.map((option) => (
-          <li
-            className="custom-select__item"
-            tabIndex={0}
-            key={`key-${option.value}`}
-            data-select-value={`id-${option.value}`}
-            aria-selected="false"
-            role="option"
-          >
-            {option.label}
-          </li>
-        ))}
+        {options.map((option) => {
+          const isSelected = option.label === selectedLabel;
 
-        <select className="custom-select__select">
+          return (
+            <li
+              className={`custom-select__item ${
+                isSelected ? "custom-select__item--selected" : ""
+              }`}
+              tabIndex={0}
+              key={`key-${option.value}`}
+              data-select-label={option.label}
+              aria-selected={isSelected}
+              role="option"
+              onClick={handleSelectClick}
+              onKeyDown={handleSelectKeydown}
+            >
+              {option.label}
+            </li>
+          );
+        })}
+
+        <select
+          className="custom-select__select visually-hidden"
+          value={selectedValue}
+        >
           {options.map((option) => (
             <option key={`key-${option.value}`} value={option.value}>
               {option.label}
